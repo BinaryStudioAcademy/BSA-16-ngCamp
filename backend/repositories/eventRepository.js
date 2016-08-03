07bb1d7aefad975cad97cf248a172335c94bcd9e
Vasyl Terletskyi <vasiafb9@gmail.com>
2016-08-03 22:23:18 +0300

    Object reference, function parameters fixed

--- backend/repositories/eventRepository.js
+++ backend/repositories/eventRepository.js
@@ -12,75 +12,41 @@ EventRepository.prototype = new Repository();
 EventRepository.prototype.addEvent = addEvent;
 EventRepository.prototype.updateEvent = updateEvent;
 
-/**
- * [[Add event to DB]]
- * @param {[[String]]} title
- * @param {[[String]]} description
- * @param {[[ObjectId]]} projectId
- * @param {[[Boolean]]} isAllDay
- * @param {[[Date]]} startDate
- * @param {[[Date]]} endDate
- * @param {[[[ObjectId]]]} partisipants
- * @param {[[[ObjectId]]]} files
- * @param {[[Function]]} callback 
- */
-function addEvent(title, description, projectId, isAllDay, startDate, endDate, partisipants, files, callback) {
+function addEvent(event, callback) {
     var model = this.model;
 
-    var event = {
-        title: title,
-        description: description,
-        project: projectId,
-        isAllDay: isAllDay,
-        startDate: startDate,
-        endDate: endDate,
-        participants: partisipants,
-        files: files
+    var document = {
+        title: event.title,
+        description: event.description,
+        project: event.projectId,
+        isAllDay: event.isAllDay,
+        startDate: event.startDate,
+        endDate: event.endDate,
+        participants: event.partisipants,
+        files: event.files
     };
 
-    var newEvent = new model(event);
+    var newEvent = new model(document);
 
     newEvent.save(callback);
 }
 //===================================================================
-/**
- * [[Update event in DB]]
- * @param {[[ObjectId]]} eventId
- * @param {[[String]]} title
- * @param {[[String]]} description
- * @param {[[ObjectId]]} projectId
- * @param {[[Boolean]]} isAllDay
- * @param {[[Date]]} startDate
- * @param {[[Date]]} endDate
- * @param {[[[ObjectId]]]} partisipants
- * @param {[[[ObjectId]]]} files
- * @param {[[Function]]} callback 
- */
-function updateEvent(eventId, title, description, projectId, isAllDay, startDate, endDate, partisipants, files, callback) {
+function updateEvent(event, callback) {
     var model = this.model;
 
     var query = {
-        _id: eventId
+        _id: event.eventId
     };
 
-    var updatedEvent = {
-        $set: {
-            title: title,
-            description: description,
-            project: projectId,
-            isAllDay: isAllDay,
-            startDate: startDate,
-            endDate: endDate,
-            participants: partisipants,
-            files: files
-        }
+    var document = {
+        $set: event.data;
     };
 
     var options = {
         multi: false
     };
 
-    model.update(query, updatedEvent, options);
+    model.update(query, document, options);
     query.exec(callback);
 }
 
