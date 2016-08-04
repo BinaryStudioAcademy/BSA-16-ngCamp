88a48c482d9bdde924bd21ff318dea8ef9e1ce28
Andrii Khanchuk <andreyblind@gmail.com>
2016-08-05 01:45:43 +0300

    uncomment user require in projectSchema

--- backend/schemas/projectSchema.js
+++ backend/schemas/projectSchema.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-//var User = require('./userSchema');
+var User = require('./userSchema');
 
 var projectSchema = new Schema({
     title: String,
     description: String,
-/*    participants: [{
+    participants: [{
         type: Schema.Types.ObjectId,
         ref: 'User'
     }],
-*/
+
     startDate: Date,
     endDate: Date,
     version: String,
