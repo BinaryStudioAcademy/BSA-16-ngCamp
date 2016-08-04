e8346237f8377e77d22632c6c4f6d6e14e730f7d
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-08-04 21:36:12 +0300

    Forbidden handler

--- frontend/services/httpGeneral.service.js
+++ frontend/services/httpGeneral.service.js
@@ -16,7 +16,8 @@
 			body: data which will be send in request,
 			errorMessageToUser: message will be shown to user when error occured,
 			errorMessageToDev: console log for debug,
-			notFoundMessage:message that will be display when Not Found
+			notFoundMessage:message that will be display when Not Found,
+			errorCallback: callback if error occured,
 		}
 	*/
 	function httpGeneral($http){
@@ -49,6 +50,10 @@
 				return res.data;
 			}	
 			function failedRequest(error){
+				if (error.status === 403){
+					handleForbidden();
+					return null;
+				}
 				if (object.errorMessageToDev) {
         			if (error.data && typeof error.data && error.data.message){
         				console.log(object.errorMessageToDev + ' ' + error.data.message);
@@ -62,6 +67,12 @@
 					} 
 					else alert(object.errorMessageToUser);
 				}
+				if (object.errorCallback != null && object.errorCallback != undefined){
+					object.errorCallback();
+				}
+			}
+			function handleForbidden(){
+				alert("You cant go here");
 			}
 		}
 	}
