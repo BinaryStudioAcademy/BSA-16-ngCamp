412aedf2640a547295076a4ed7fd1533bb758d9a
Dmitriy Beseda <BesedaDG@gmail.com>
2016-08-04 07:47:15 +0300

    Added setObjPropsById to general repository to implement update of necessary properties by entityId

--- backend/repositories/generalRepository.js
+++ backend/repositories/generalRepository.js
@@ -9,34 +9,44 @@ Repository.prototype.addBatch = addBatch;
 Repository.prototype.deleteById = deleteById;
 Repository.prototype.getAll = getAll;
 Repository.prototype.getById = getById;
+Repository.prototype.setObjPropsById = setObjPropsById;
 
 function add(data, callback) {
-	var model = this.model;
-	var newItem = new model(data);
-	newItem.save(callback);
+    var model = this.model;
+    var newItem = new model(data);
+    newItem.save(callback);
 }
 
 function addBatch(batchToInsert, callback) {
-	var query = this.model.create(batchToInsert, callback);
+    var query = this.model.create(batchToInsert, callback);
 }
 
 function deleteById(id, callback) {
-	var query = this.model.remove({
-		_id: id
-	});
-	query.exec(callback);
+    var query = this.model.remove({
+        _id: id
+    });
+    query.exec(callback);
 }
 
 function getAll(callback) {
-	var query = this.model.find({});
-	query.exec(callback);
+    var query = this.model.find({});
+    query.exec(callback);
 }
 
 function getById(id, callback) {
-	var query = this.model.findOne({
-		_id: id
-	});
-	query.exec(callback);
+    var query = this.model.findOne({
+        _id: id
+    });
+    query.exec(callback);
+}
+
+function setObjPropsById(id, setObj, callback) {
+    var query = this.model.update({
+        _id: id
+    }, {
+        $set: setObj
+    });
+    query.exec(callback);
 }
 
 module.exports = Repository;
