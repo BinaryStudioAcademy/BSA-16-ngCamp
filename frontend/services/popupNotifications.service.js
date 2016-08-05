a660517ce2f923da7a52241021afcf9cc1a65de3
Ivan Stanko <ivan_stanko@ukr.net>
2016-08-05 13:05:12 +0300

    #18 - modified notifyConfirm and add reset

--- frontend/services/popupNotifications.service.js
+++ frontend/services/popupNotifications.service.js
@@ -1,88 +1,83 @@
-(function () {
-  "use strict";
+import angular from "angular";
 
-  angular
+angular
     .module("base")
     .factory("popupNotifications", popupNotifications);
 
-  popupNotifications.$inject = ["Alertify"];
+popupNotifications.$inject = ["Alertify"];
 
-  function popupNotifications(Alertify) {
+function popupNotifications(Alertify) {
     let service = {
-      notifyAlert: notifyAlert,
-      notifyConfirm: notifyConfirm,
-      notifySuccess: notifySuccess,
-      notifyStandard: notifyStandard,
-      notifyError: notifyError,
-      persistentSuccess: persistentSuccess,
-      persistentStandard: persistentStandard,
-      persistentError: persistentError
+        notifyAlert: notifyAlert,
+        notifyConfirm: notifyConfirm,
+        notifySuccess: notifySuccess,
+        notifyStandard: notifyStandard,
+        notifyError: notifyError,
+        persistentSuccess: persistentSuccess,
+        persistentStandard: persistentStandard,
+        persistentError: persistentError,
+        reset: reset
     };
 
-    return service;
-
     function notifyAlert(alertMsg) {
-      alertMsg = alertMsg || "Alert Dialog!";
-      Alertify.alert(alertMsg);
+        reset();
+        alertMsg = alertMsg || "Alert Dialog!";
+        Alertify.alert(alertMsg);
     }
 
-    function notifyConfirm(confirmMsg) {
-      confirmMsg = confirmMsg || "Are you sure?";
-
-      Alertify.set({
-        buttonReverse: true,
-        labels: {
-          ok: "Accept",
-          cancel: "Deny"
-        }
-      });
-
-      Alertify.confirm(confirmMsg)
-        .then(function () {
-          return notifySuccess("You've clicked OK");
-        })
-        .catch(function () {
-          return notifyError("You've clicked Cancel");
+    function reset() {
+        Alertify.set({
+            buttonReverse: true,
+            labels: {
+                ok: "OK",
+                cancel: "Cancel"
+            }
         });
     }
 
+    function notifyConfirm(confirmMsg, btnOk, btnCancel) {
+        confirmMsg = confirmMsg || "Are you sure?";
+
+        Alertify.set({
+            buttonReverse: true,
+            labels: {
+                ok: btnOk ? btnOk : "Ok",
+                cancel: btnCancel ? btnCancel : "Cancel"
+            }
+        });
+
+        Alertify.confirm(confirmMsg);
+    }
+
     function notifySuccess(successMsg) {
-      successMsg = successMsg || "Success log message";
-      Alertify.success(successMsg);
+        successMsg = successMsg || "Success log message";
+        Alertify.success(successMsg);
     }
 
     function notifyStandard(standardMsg) {
-      standardMsg = standardMsg || "Standard log message";
-      Alertify.log(standardMsg);
+        standardMsg = standardMsg || "Standard log message";
+        Alertify.log(standardMsg);
     }
 
     function notifyError(errorMsg) {
-      errorMsg = errorMsg || "Error log message";
-      Alertify.error(errorMsg);
+        errorMsg = errorMsg || "Error log message";
+        Alertify.error(errorMsg);
     }
 
     function persistentSuccess(successPersist) {
-      successPersist = successPersist || "Will stay untill clicked";
-      alertify.success(successPersist, 0);
+        successPersist = successPersist || "Will stay untill clicked";
+        alertify.success(successPersist, 0);
     }
 
     function persistentStandard(standardPersist) {
-      standardPersist = standardPersist || "Will stay untill clicked";
-      alertify.log(standardPersist, "", 0);
+        standardPersist = standardPersist || "Will stay untill clicked";
+        alertify.log(standardPersist, "", 0);
     }
 
     function persistentError(errorPersist) {
-      errorPersist = errorPersist || "Will stay untill clicked";
-      alertify.error(errorPersist, 0);
+        errorPersist = errorPersist || "Will stay untill clicked";
+        alertify.error(errorPersist, 0);
     }
 
-    // Prompt Dialog
-    // Alertify.prompt("Your age", 21).then(
-    //   function onOk(answer) {},
-    //   function onCancel() {}
-    // );
-
-    // Pops dialog with JSON of the object
-    // Alertify.json(object);
-  }
-})();
+    return service;
+}
