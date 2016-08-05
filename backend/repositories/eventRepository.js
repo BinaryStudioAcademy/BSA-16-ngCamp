692c0b23d9628835fda056d91c0e33e3708c0bbc
Vasyl Terletskyi <vasiafb9@gmail.com>
2016-08-05 10:57:10 +0300

    Remove updateEvent function

--- backend/repositories/eventRepository.js
+++ backend/repositories/eventRepository.js
@@ -8,25 +8,4 @@ function EventRepository() {
 
 EventRepository.prototype = new Repository();
 
-EventRepository.prototype.updateEvent = updateEvent;
-//===================================================================
-function updateEvent(event, callback) {
-    var model = this.model;
-
-    var query = {
-        _id: event.eventId
-    };
-
-    var document = {
-        $set: event.data;
-    };
-
-    var options = {
-        multi: false
-    };
-
-    model.update(query, document, options);
-    query.exec(callback);
-}
-
 model.exports = new EventRepository();
