4f7a7fa485acf397aa8a9b3d43b08a18023b3003
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-08-05 13:13:26 +0300

    format code

--- frontend/services/httpGeneral.factory.js
+++ frontend/services/httpGeneral.factory.js
@@ -18,65 +18,65 @@ httpGeneral.$inject = [
 		errorCallback: callback if error occured,
 	}
 */
-let factory = {httpGeneral: httpGeneral};
 
 function httpGeneral($http) {
-    return {
-        httpSend: function(object) {
-            if (typeof object.url !== 'string' || object.url === undefined || object.url.length === 0) {
-                throw "HTTP REQUEST EROR: REQUEST ULR IS ABSENT";
-                return;
-            }
-            if (typeof object.type !== 'string' || object.type === undefined || object.type.length === 0) {
-                throw "HTTP REQUEST EROR: REQUEST TYPE IS ABSENT";
-                return;
-            }
-            switch (object.type.toLowerCase()) {
-                case 'get':
-                    return $http.get(object.url).then(succesfullRequest, failedRequest);
-                    break;
-                case 'post':
-                    return $http.post(object.url, object.body).then(succesfullRequest, failedRequest);
-                    break;
-                case 'put':
-                    return $http.put(object.url, object.body).then(succesfullRequest, failedRequest);
-                    break;
-                case 'delete':
-                    return $http.delete(object.url).then(succesfullRequest, failedRequest);
-                    break;
-            }
+    let factory = {httpSend: httpSend};
 
-            function succesfullRequest(res) {
-                //console.log("Succesfull Request");
-                //alert(object.successMessageToUser);
-                return res.data;
-            }
+    function httpSend(object) {
+        if (typeof object.url !== 'string' || object.url === undefined || object.url.length === 0) {
+            throw "HTTP REQUEST EROR: REQUEST ULR IS ABSENT";
+            return;
+        }
+        if (typeof object.type !== 'string' || object.type === undefined || object.type.length === 0) {
+            throw "HTTP REQUEST EROR: REQUEST TYPE IS ABSENT";
+            return;
+        }
+        switch (object.type.toLowerCase()) {
+            case 'get':
+                return $http.get(object.url).then(succesfullRequest, failedRequest);
+                break;
+            case 'post':
+                return $http.post(object.url, object.body).then(succesfullRequest, failedRequest);
+                break;
+            case 'put':
+                return $http.put(object.url, object.body).then(succesfullRequest, failedRequest);
+                break;
+            case 'delete':
+                return $http.delete(object.url).then(succesfullRequest, failedRequest);
+                break;
+        }
 
-            function failedRequest(error) {
-                if (error.status === 403) {
-                    handleForbidden();
-                    return null;
-                }
-                if (object.errorMessageToDev) {
-                    if (error.data && error.data.message) {
-                        console.log(object.errorMessageToDev + ' ' + error.data.message);
-                    } else console.log(object.errorMessageToDev + ' ' + error.data);
-                }
-                if (object.errorMessageToUser) {
-                    if (error.status === 404 && (typeof error.data === "string" && error.data.indexOf('Cannot GET /api/') === -1)) {
-                        if (object.notFoundMessage) console.log(object.notFoundMessage);
-                        else alert(object.errorMessageToUser);
-                    } else alert(object.errorMessageToUser);
-                }
-                if (object.errorCallback != null && object.errorCallback != undefined) {
-                    object.errorCallback();
-                }
-            }
+        function succesfullRequest(res) {
+            //console.log("Succesfull Request");
+            //alert(object.successMessageToUser);
+            return res.data;
+        }
 
-            function handleForbidden() {
-                alert("You cant go here");
+        function failedRequest(error) {
+            if (error.status === 403) {
+                handleForbidden();
+                return null;
             }
+            if (object.errorMessageToDev) {
+                if (error.data && error.data.message) {
+                    console.log(object.errorMessageToDev + ' ' + error.data.message);
+                } else console.log(object.errorMessageToDev + ' ' + error.data);
+            }
+            if (object.errorMessageToUser) {
+                if (error.status === 404 && (typeof error.data === "string" && error.data.indexOf('Cannot GET /api/') === -1)) {
+                    if (object.notFoundMessage) console.log(object.notFoundMessage);
+                    else alert(object.errorMessageToUser);
+                } else alert(object.errorMessageToUser);
+            }
+            if (object.errorCallback != null && object.errorCallback != undefined) {
+                object.errorCallback();
+            }
+        }
+
+        function handleForbidden() {
+            alert("You cant go here");
         }
     }
 }
 return factory;
+}
