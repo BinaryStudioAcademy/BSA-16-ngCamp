ef832716f41a0e1b7049224276d92cb6a51cba2a
Vasyl Terletskyi <vasiafb9@gmail.com>
2016-08-05 10:18:24 +0300

    Inject data url bug

--- backend/middleware/injectedDataMiddleware.js
+++ backend/middleware/injectedDataMiddleware.js
@@ -1,25 +1,25 @@
 var fs = require('fs'),
-	replaceStream = require('replacestream');
+    replaceStream = require('replacestream');
 
-module.exports = function(req, res, obj, error) {
-	error = error || false;
+module.exports = function (req, res, obj, error) {
+    error = error || false;
 
-	populateInjectData(req.user, function(data) {
-		if (req.user !== null && req.user !== undefined) {
-			obj.isLoggedIn = true;
-			obj.userId = req.user._id;
-			obj.role = req.user.role;
-		}
+    populateInjectData(req.user, function (data) {
+        if (req.user !== null && req.user !== undefined) {
+            obj.isLoggedIn = true;
+            obj.userId = req.user._id;
+            obj.role = req.user.role;
+        }
 
-		res.header = ('Content-Type', 'text/html');
-		fs.createReadStream(__dirname + '/../../public/' + '_index.html')
-			.pipe(replaceStream("[\"data_replace\"]", JSON.stringify(obj).replace(/'/g, "\\'").replace(/\\\"/g, "\\\\\"")))
-			.pipe(replaceStream("window._is404Error = false;", "window._is404Error = " + error + ";"))
-			.pipe(res);
+        res.header = ('Content-Type', 'text/html');
+        fs.createReadStream(__dirname + '/../../public/' + 'index.html')
+            .pipe(replaceStream("[\"data_replace\"]", JSON.stringify(obj).replace(/'/g, "\\'").replace(/\\\"/g, "\\\\\"")))
+            .pipe(replaceStream("window._is404Error = false;", "window._is404Error = " + error + ";"))
+            .pipe(res);
 
-	});
+    });
 
-	function populateInjectData(user, callback_main) {
-		callback_main(null, null);
-	}
+    function populateInjectData(user, callback_main) {
+        callback_main(null, null);
+    }
 };
