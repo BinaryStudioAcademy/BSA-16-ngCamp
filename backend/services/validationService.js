ed19af08d1e9a6183cde72b6ab67c0bf1bf76807
Vasyl Terletskyi <vasiafb9@gmail.com>
2016-08-05 13:00:23 +0300

    Init event service

--- backend/services/validationService.js
+++ backend/services/validationService.js
@@ -4,6 +4,7 @@ function ValidationService() {
 
 ValidationService.prototype.validationBodyProperty = validationBodyProperty;
 ValidationService.prototype.addUserValidation = addUserValidation;
+ValidationService.prototype.addEventValidation = addEventValidation;
 
 function addUserValidation(body, callback) {
     if (!body.firstName && !body.lastName) {
@@ -24,16 +25,16 @@ function addUserValidation(body, callback) {
         }, null);
         return false;
     }
-    if (body.password.length <= 6) {        
-            callback({
-                message: 'Password should be more than 6 chars'
-            }, null);
-            return false;        
+    if (body.password.length <= 6) {
+        callback({
+            message: 'Password should be more than 6 chars'
+        }, null);
+        return false;
     }
     return true;
 }
 
-function validationBodyProperty(body, propName, callback) {   
+function validationBodyProperty(body, propName, callback) {
     if (!body || !body[propName]) {
         if (callback) {
             callback({
@@ -44,7 +45,39 @@ function validationBodyProperty(body, propName, callback) {
     }
     return true;
 }
+//=========================================================
+function addEventValidation(body, callback) {
+    if (!body.title) {
+        callback({
+            message: "Event title is undefined"
+        });
+        return false;
+    }
+    if (!body.project) {
+        callback({
+            message: "Project id is undefined"
+        });
+        return false;
+    }
+    if (!body.startDate) {
+        callback({
+            message: "Start date is undefined"
+        });
+        return false;
+    }
+    if (!body.endDate) {
+        callback({
+            message: "End date is undefined"
+        });
+        return false;
+    }
+    if (body.participants.length <= 0 || !body.participants) {
+        callback({
+            message: "Nobody take a part in event"
+        });
+        return false;
+    }
 
-
+}
 
 module.exports = new ValidationService();
