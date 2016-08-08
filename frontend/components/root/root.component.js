919cddc5acafebc1438db928e28ea75d399f4298
AnthStep <lightlife25@gmail.com>
2016-08-08 15:42:14 +0300

    add top routes to route component Signed-off-by: AnthStep <lightlife25@gmail.com>

--- frontend/components/root/root.component.js
+++ frontend/components/root/root.component.js
@@ -15,7 +15,44 @@ const rootComponent = {
 		path: '/**',
 		name: 'NotFound',
 		component: 'notFound'
-	}]
+	}, 	{
+		path: '/mainpage/',
+		name: 'MainPage',
+		component: 'mainComponent'
+	},	{
+		path: '/account/',
+		name: 'Account',
+		component: 'accountComponent'
+	},	{
+		path: '/checkins/',
+		name: 'Checkins',
+		component: 'checkinsComponent'
+	},	{
+		path: '/tasks/',
+		name: 'Tasks',
+		component: 'tasksComponent'
+	},	{
+		path: '/events/',
+		name: 'Events',
+		component: 'eventsComponent'
+	},	{
+		path: '/project/',
+		name: 'Project',
+		component: 'projectComponent'
+	},	{
+		path: '/trash/',
+		name: 'Trash',
+		component: 'mainComponent'
+	},	{
+		path: '/main/', 
+		name: 'Reports',
+		component: 'reportsComponent'
+	},	{
+		path: '/drafts/',
+		name: 'Drafts',
+		component: 'draftsComponent'
+	}
+	]
 };
 
 export {
