fe4813ddf50311598fe42f32d43202ce8e57d1a5
Dmitriy Beseda <besedadg@gmail.com>
2016-08-08 16:12:29 +0300

    Added angular component router from node_modules instead of direct link

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import ngComponentRouter from 'ngcomponentrouter';
 const app = angular.module('base', ['ngComponentRouter']);
 
 app.value('$routerRootComponent', 'rootElement');
