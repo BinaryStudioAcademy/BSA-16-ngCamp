c218164da4b5a3bd74fb7dd87aaf2ddadbd5894e
Ivan Stanko <ivan_stanko@ukr.net>
2016-08-09 09:41:37 +0300

    #43 - add example Checkin markup

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -2,7 +2,9 @@ import {app} from '../index';
 import {notFoundComponent} from './notFound.component.js';
 import {userComponent} from './user/user.component.js';
 import {rootComponent} from './root/root.component.js';
+import {checkinsComponent} from './checkins/checkins.component.js';
 
 app.component(rootComponent.selector, rootComponent);
 app.component(userComponent.selector, userComponent);
+app.component(checkinsComponent.selector, checkinsComponent);
 app.component(notFoundComponent.selector, notFoundComponent);
