c1e948a60e20df6339e0fdba249036ddfc7878f9
AnthStep <lightlife25@gmail.com>
2016-08-09 17:07:33 +0300

    markup for main page Signed-off-by: AnthStep <lightlife25@gmail.com>

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -2,7 +2,9 @@ import {app} from '../index';
 import {notFoundComponent} from './notFound.component.js';
 import {userComponent} from './user/user.component.js';
 import {rootComponent} from './root/root.component.js';
+import {mainComponent} from './main/main.component.js';
 
 app.component(rootComponent.selector, rootComponent);
 app.component(userComponent.selector, userComponent);
 app.component(notFoundComponent.selector, notFoundComponent);
+app.component(mainComponent.selector, mainComponent);
