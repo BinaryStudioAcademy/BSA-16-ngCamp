da8983b09db25b7364a6223a28f0432dda1bc167
Dmitriy Beseda <besedadg@gmail.com>
2016-08-10 18:16:09 +0300

    make main route by / address + comment report route in root component

--- frontend/components/root/root.component.js
+++ frontend/components/root/root.component.js
@@ -8,59 +8,61 @@ const rootComponent = {
     selector: 'rootElement',
     template: '<ng-outlet></ng-outlet>',
     $routeConfig: [{
-        path: '/user/',
-        name: 'User',
-        component: 'userComponent'
-    }, {
-        path: '/mainpage/',
-        name: 'MainPage',
-        component: 'mainComponent',
-        useAsDefault: true
-    }, {
-        path: '/account/',
-        name: 'Account',
-        component: 'accountComponent'
-    }, {
-        path: '/checkins/',
-        name: 'Checkins',
-        component: 'checkinsComponent'
-    }, {
-        path: '/tasks/',
-        name: 'Tasks',
-        component: 'tasksComponent'
-    }, {
-        path: '/events/...',
-        name: 'Events',
-        component: 'eventsComponent'
-    }, {
-        path: '/project/',
-        name: 'Project',
-        component: 'projectComponent'
-    }, {
-        path: '/trash/',
-        name: 'Trash',
-        component: 'trashComponent'
-    }, {
-        path: '/main/',
-        name: 'Reports',
-        component: 'reportsComponent'
-    }, {
-        path: '/drafts/',
-        name: 'Drafts',
-        component: 'draftsComponent'
-    }, {
-        path: '/messageboard',
-        name: 'MsgBoard',
-        component: 'messageBoard'
-    }, {
-        path: '/comments',
-        name: 'Comments',
-        component: 'commentsComponent'
-    }, {
-        path: '/**',
-        name: 'NotFound',
-        component: 'notFound'
-    }]
+            path: '/user/',
+            name: 'User',
+            component: 'userComponent'
+        }, {
+            path: '/',
+            name: 'MainPage',
+            component: 'mainComponent',
+        }, {
+            path: '/account/',
+            name: 'Account',
+            component: 'accountComponent'
+        }, {
+            path: '/checkins/',
+            name: 'Checkins',
+            component: 'checkinsComponent'
+        }, {
+            path: '/tasks/',
+            name: 'Tasks',
+            component: 'tasksComponent'
+        }, {
+            path: '/events/...',
+            name: 'Events',
+            component: 'eventsComponent'
+        }, {
+            path: '/project/',
+            name: 'Project',
+            component: 'projectComponent'
+        }, {
+            path: '/trash/',
+            name: 'Trash',
+            component: 'trashComponent'
+        },
+        // {
+        //     path: '/reports/',
+        //     name: 'Reports',
+        //     component: 'reportsComponent'
+        // }, 
+        {
+            path: '/drafts/',
+            name: 'Drafts',
+            component: 'draftsComponent'
+        }, {
+            path: '/messageboard',
+            name: 'MsgBoard',
+            component: 'messageBoard'
+        }, {
+            path: '/comments',
+            name: 'Comments',
+            component: 'commentsComponent'
+        }, {
+            path: '/**',
+            name: 'NotFound',
+            component: 'notFound'
+        }
+    ]
 };
 
 export {
