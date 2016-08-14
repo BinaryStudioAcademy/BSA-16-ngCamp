5054b6363849d61f4287f911d4404ab4b7361fcb
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-08-14 15:32:04 +0300

    Client side change project

--- frontend/components/project/project.component.js
+++ frontend/components/project/project.component.js
@@ -1,17 +1,34 @@
 import './project.component.styl';
 
 class ProjectComponentController {
-    constructor(popupNotifications) {
+    constructor(popupNotifications, httpGeneral) {
         this.popupNotifications = popupNotifications;
+        this.httpGeneral = httpGeneral;
+        this.user = {
+            type: "GET",
+            url: "api/user/me",
+        };
+        this.projects = [];
+        this.flag = true;
+        this.id = null;
+    }
+    getProjects() {
+        let self = this;
+        self.flag = false;
+        self.httpGeneral.sendRequest(self.user).then(function(res) {
+            console.log(res);
+            self.projects=res.projects;
+        });
     }
 }
 
-ProjectComponentController.$inject = ['popupNotifications'];
+ProjectComponentController.$inject = ['popupNotifications', 'httpGeneral'];
 
 const projectComponent = {
     controller: ProjectComponentController,
     selector: 'projectComponent',
     template: require('./project.component.pug')(),
+    controllerAs: 'proj',
 };
 
 
