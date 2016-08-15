c03f10d18b80258d45215e7b4bd039afae7164ed
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-08-15 14:26:24 +0300

    PUT Request for change current project

--- frontend/components/project/project.component.js
+++ frontend/components/project/project.component.js
@@ -13,25 +13,44 @@ class ProjectComponentController {
         this.projects = [];
         this.flag = true;
         this.id = null;
+        self.curentProjectId;
+        self.curentProjectTitle;
     }
     getProjects() {
         let self = this;
         self.flag = false;
         self.httpGeneral.sendRequest(self.userReq).then(function(res) {
             self.user = res;
-            console.log( res );
             self.projectsId = res.projects;
-            self.projectsId.forEach(function(item){
-                self.httpGeneral.sendRequest({type: 'get',url: `api/projects/${item}`}).then(function(res){
+            self.projectsId.forEach(function(item) {
+                self.httpGeneral.sendRequest({
+                    type: 'get',
+                    url: `api/projects/${item}`
+                }).then(function(res) {
                     self.projects.push(res);
                 });
             });
         });
     }
 
-    setProject(){
+    setProject() {
         let self = this;
-
+        self.curentProjectId;
+        for (let proj in self.projects) {
+            if (self.projects[proj].title === self.curentProjectTitle) {
+                self.curentProjectId = self.projects[proj]._id;
+                self.user.curentProject = self.projects[proj]._id;
+            }
+        };
+        self.httpGeneral.sendRequest({
+            type: "PUT",
+            url: `api/user/${self.user._id}`,
+            body: {
+                curentProject: self.curentProjectId,
+            }
+        }).then(function(res) {
+            //console.log("Succesfull update");
+        });
     }
 }
 
