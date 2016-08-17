77ed7580957064a02a8bd93f885b52894ff4a551
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-08-17 23:35:23 +0300

    create controller for create project page

--- frontend/components/createProject/createProject.component.js
+++ frontend/components/createProject/createProject.component.js
@@ -1,14 +1,65 @@
 import './createProject.component.styl';
 
 class createProjectController {
-	constructor(http) {
-		this.http = http;
+	constructor(httpGeneral,$location) {
+		this.http = httpGeneral;
+		this.projectTitle;
+		this.projectDescription;
+		this.participants = [],
+		this.users;
+		this.status = "active";
+		this.popup = {
+            opened: false
+        };
+        this.today;
+        this.deadline = new Date();
+        this.modalFlag = false;
 	}
 
+	$onInit() {
+        let self = this;
+        self.projectParticipants = [];
+        self.httpGeneral.sendRequest({
+            type: "GET",
+            url: "api/user"
+        }).then(function(res) {
+            self.users = res;
+        });
+    }
+
+	save(){
+		let self = this;
+		self.httpGeneral.sendRequest({
+			type: "POST",
+			url: "api/porjects",
+			body: {
+				data: {
+					title:self.projectTitle,
+					description:self.projectDescription,
+					participants:self.participants,
+					endDate:self.deadline,
+					startDate: new Date(),
+				}
+			}
+		}).then(function(res){
+			console.log("Succesfull create project");
+		});
+	}
+
+	modalToggle() {
+        this.modalFlag = !this.modalFlag;
+    }
+    today () {
+        this.dt = new Date();
+    };
+    open() {
+        this.popup.opened = true;
+    }
 }
 
 createProjectController.$inject = [
-	'httpGeneral'
+	'httpGeneral',
+	'$location',
 ];
 
 const createProjectComponent = {
