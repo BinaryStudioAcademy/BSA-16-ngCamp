62ad1b3583141cc49ecce4a65a7b3c4f3669bb9b
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-08-17 12:49:51 +0300

    Merge conflickt resolved

--- backend/schemas/projectSchema.js
+++ backend/schemas/projectSchema.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var User = require('./userSchema');
 var Task = require('./taskSchema');
 var Event = require('./eventSchema');
 var Message = require('./messageSchema');
-
+var User = require('./userSchema.js');
 
 
 var projectSchema = new Schema({
@@ -17,53 +16,61 @@ var projectSchema = new Schema({
     startDate: Date,
     endDate: Date,
     version: String,
-    repository: String
+    repository: String,
+    status: {
+        type: String,
+        enum: ['active','finished','deleted']
+    }
 });
 
 
-projectSchema.pre('remove', function(next) {
-    var tasks = Task.find({
-        project: this._id
-    }, function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    tasks.then(function(tasks) {
-        tasks.forEach(function(task) {
-            task.remove();
-        });
-    });
-    next();
-});
+// projectSchema.pre('remove', function(next) {
+//     var tasks = Task.find({
+//         project: this._id
+//     }, function(err, data) {
+//         if (err) {
+//             console.log(err);
+//         }
+//     });
+//     tasks.then(function(tasks) {
+//         tasks.forEach(function(task) {
+//             task.remove();
+//         });
+//     });
+//     next();
+// });
 
-projectSchema.pre('remove', function(next) {
-    User.update({
-        projects: {
-            $in: [this._id]
-        }
-    }, {
-        $pull: {
-            projects: this._id
-        },
-        $set: {
-            curentProject: this._id
-        }
-    }, {
-        multi: true
-    }).exec();
-    next();
-});
+// projectSchema.pre('remove', function(next) {
+//     var user = User;
+//     var conditions = {
+//         projects: {
+//             $in: [this._id]
+//         }
+//     };
+//     var update = {
+//         $pull: {
+//             projects: this._id
+//         },
+//         $set: {
+//             curentProject: this._id
+//         }
+//     };
+//     var options = {
+//         multi: true
+//     };
+
+//     console.log(user.update);
+// });
 
 
-projectSchema.pre('remove', function(next) {
-    Event.remove({
-        project: this._id
-    }, function(err, obj) {}).exec();
-    Message.remove({
-        project: this._id
-    }, function(err, obj) {}).exec();
-    next();
-});
+// projectSchema.pre('remove', function(next) {
+//     Event.remove({
+//         project: this._id
+//     }, function(err, obj) {}).exec();
+//     Message.remove({
+//         project: this._id
+//     }, function(err, obj) {}).exec();
+//     next();
+// });
 
 module.exports = mongoose.model('Project', projectSchema);
