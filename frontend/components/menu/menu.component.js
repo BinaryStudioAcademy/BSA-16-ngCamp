06a4009f3565aa1425067835416365287dcf522a
Yevhen Verba <verba_e_k@mail.ru>
2016-08-18 10:48:52 +0300

    initial

--- frontend/components/menu/menu.component.js
+++ frontend/components/menu/menu.component.js
@@ -4,6 +4,23 @@ class MenuComponentController {
     constructor(http) {
         this.http = http;
     }
+    showMenu(){
+
+    	let x = document.getElementById("side-menu");
+    	if (x.className === "side-menu") {
+        	x.className += " show";
+    	} else {
+          	x.className = "side-menu";
+    	}
+    	// console.log(document.getElementsByClassName("side-menu").style.display);
+    	// console.log(a.style.display);
+    	// if (a.style.display == "none" ) {
+    	// 	a.style.display = block;
+    	// } else {
+    	// 	a.style.displaly = none;
+    	// }
+    	// console.log(a);
+    }
 }
 
 MenuComponentController.$inject = ["httpGeneral"];
