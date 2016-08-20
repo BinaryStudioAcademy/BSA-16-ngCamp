08cbb2353ee1a006e868993fefe6b7214db41fb7
Bender <bender.d1mon@gmail.com>
2016-08-20 22:27:37 +0300

    day calendar start, header finish (no style)

--- frontend/components/test/test.component.js
+++ frontend/components/test/test.component.js
@@ -2,12 +2,27 @@ import './test.styl';
 
 class testCtrl {
     constructor() {
+        let vm = this;
+        vm.alert = () => alert('test');
+
+        vm.currentDate = new Date();
+        vm.isChangeDate = false;
+        vm.startChangeDate = () => vm.isChangeDate = true;
+        vm.endChangeDate = () => vm.isChangeDate = false;
+        vm.changeDateOfStep = (step) => {
+            let date = new Date(vm.currentDate);
+            date.setDate(date.getDate() + step);
+            vm.currentDate = date;
+        };
     }
 }
 
 const testComponent = {
     controller: testCtrl,
-    controllerAs: 'test',
+    controllerAs: 'ts',
+    bindings: {
+        //isChangeDate: '<'
+    },
     template: require('./test.pug')(),
     selector: 'test'
 };
