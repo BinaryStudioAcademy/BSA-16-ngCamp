2a825b7e341da98ecf4ae968fc68b673f89d5369
Andrii Khanchuk <andreyblind@gmail.com>
2016-08-23 16:33:36 +0300

    refactor resizable directive

--- frontend/directives/index.js
+++ frontend/directives/index.js
@@ -2,6 +2,8 @@ import {app} from '../index';
 
 import {timeIndicatorDirective} from './timeIndicator/timeIndicator.directive.js';
 import {readMoreDirective} from './readMore/readMore.directive.js';
+import {angularResizableDirective} from "./angularResizable/angularResizable.directive.js";
 
 app.directive(timeIndicatorDirective.name, () => timeIndicatorDirective);
 app.directive(readMoreDirective.name, () => readMoreDirective);
+app.directive('resizable', angularResizableDirective);
