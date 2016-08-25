bea6dc0f10bb81d697539d75794370356b21763d
AnthStep <lightlife25@gmail.com>
2016-08-25 11:07:52 +0300

    Some task component update + creating of user list directive Signed-off-by: AnthStep <lightlife25@gmail.com>

--- frontend/directives/index.js
+++ frontend/directives/index.js
@@ -2,6 +2,8 @@ import {app} from '../index';
 
 import {timeIndicatorDirective} from './timeIndicator/timeIndicator.directive.js';
 import {readMoreDirective} from './readMore/readMore.directive.js';
+import {userListDirective} from './userList/userList.directive.js';
 
 app.directive(timeIndicatorDirective.name, () => timeIndicatorDirective);
 app.directive(readMoreDirective.name, () => readMoreDirective);
+app.directive(userListDirective.name, () => userListDirective);
