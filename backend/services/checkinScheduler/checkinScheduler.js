c0b0afa22cbd39a04ef47bc89555d2433353dfc5
Yevhen Verba <verba_e_k@mail.ru>
2016-08-29 16:56:52 +0300

    with comments

--- backend/services/checkinScheduler/checkinScheduler.js
+++ backend/services/checkinScheduler/checkinScheduler.js
@@ -2,18 +2,40 @@ var schedule = require('node-schedule');
 var currentWeekNumber = require('current-week-number');
 var scheduleCheckinsByFrequency = require('./scheduleCheckin');
 
-var everyMonday = schedule.scheduleJob('*/2 * * * * *', function (){
-	console.log('every what');
-	scheduleCheckinsByFrequency('Every Monday')
-});
+function timeToCompare(){
+	var currentDate = new Date();
+	var dayOfWeek = currentDate.getDay();
+    var hour = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    return  {time: hour + ':' + minutes, dayOfWeek: dayOfWeek};
+}
+
+// var everyMonday = schedule.scheduleJob('* * * * 1', function (){
+//     var time = timeToCompare();
+// 	scheduleCheckinsByFrequency('Every Monday', time);
+// });
 
 
 // var everyMonday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 1}, function (){
 // 	scheduleCheckinsByFrequency('Every Monday')
 // });
 
+// var everyFriday = schedule.scheduleJob('* * * * 5', function(){
+// 	var time = timeToCompare();
+// 	scheduleCheckinsByFrequency('Every Friday', time);
+// });
+
+
 // var everyFriday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 5}, function(){
 // 	scheduleCheckinsByFrequency('Every Friday')
+// });
+
+
+// var everyOtherMonday = schedule.scheduleJob('* * * * 1', function(){
+// 	if (currentWeekNumber()%2 === 0){
+// 		scheduleCheckinsByFrequency('Every other Monday');
+// 	}
+	
 // });
 
 // var everyOtherMonday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 1}, function(){
@@ -23,6 +45,13 @@ var everyMonday = schedule.scheduleJob('*/2 * * * * *', function (){
 	
 // });
 
+// var everyOtherFriday = schedule.scheduleJob('* * * * 5', function() {
+// 	var time = timeToCompare();
+// 	if (currentWeekNumber()%2 === 0){
+// 		scheduleCheckinsByFrequency('Every other Friday', time);
+// 	}
+// });
+	
 // var everyOtherFriday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 5}, function() {
 // 	if (currentWeekNumber()%2 === 0){
 // 		scheduleCheckinsByFrequency('Every other Friday');
@@ -30,29 +59,46 @@ var everyMonday = schedule.scheduleJob('*/2 * * * * *', function (){
 	
 // });
 
+var firstMondayOfEveryMonth = schedule.scheduleJob('* * * 1-7 1', function() {
+	var time = timeToCompare();
+	scheduleCheckinsByFrequency('First Monday of every month', time);
+});
+
+
 // var firstMondayOfEveryMonth = schedule.scheduleJob({hour: 6, minute: 30, dayOfMonth: [new schedule.Range(1, 7)], dayOfWeek: 1}, function() {
 // 	scheduleCheckinsByFrequency('First Monday of every month');
 // });
+// var everyWeekday = schedule.scheduleJob('* * * * 1-5', function(){
+var everyWeekday = schedule.scheduleJob('*/2 * * * * *', function(){
+	var dayAndTime = timeToCompare();
+	if(dayAndTime.dayOfWweek === 1){
+		scheduleCheckinsByFrequency('Every Monday', dayAndTime.time);
+		if (currentWeekNumber()%2 === 0){
+			scheduleCheckinsByFrequency('Every other Monday');
+		}
+	} else if(dayAndTime.dayOfWweek === 5){
+		scheduleCheckinsByFrequency('Every Friday', dayAndTime.time);
+		if (currentWeekNumber()%2 === 0){
+			scheduleCheckinsByFrequency('Every other Friday', dayAndTime.time);
+		}
+	}
+	scheduleCheckinsByFrequency('Every weekday', dayAndTime.time);
+});
 
 // var everyWeekday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: [new schedule.Range(1, 5)] }, function(){
 // 	scheduleCheckinsByFrequency('Every weekday')
 // });
 
-var test = schedule.scheduleJob('*/2 * * * * *', function(){
-	var currentDate = new Date();
-    var dayOfWeek = currentDate.getDay();
-	console.log('sdfsdfsdf');
-	scheduleCheckinsByFrequency('Every Monday');
-});
+// var test = schedule.scheduleJob('*/2 * * * * *', function(){
+// 	var currentDate = new Date();
+//     var dayOfWeek = currentDate.getDay();
+// 	console.log('sdfsdfsdf');
+// 	scheduleCheckinsByFrequency('Every Monday');
+// });
 
 
 
 module.exports = {
-	everyMonday: everyMonday,
-	// everyFriday: everyFriday,
-	// everyOtherMonday: everyOtherMonday,
-	// everyOtherFriday: everyOtherFriday,
-	// firstMondayOfEveryMonth: firstMondayOfEveryMonth,
-	// everyWeekday: everyWeekday,
-	test: test
+	everyWeekday: everyWeekday
+	// test: test
 };
--- backend/services/checkinScheduler/scheduleCheckin.js
+++ backend/services/checkinScheduler/scheduleCheckin.js
@@ -1,25 +1,27 @@
 var sendEmailToCheckinPartisipants = require('./sendEmail');
 var repo = require('../../repositories/checkinRepository');
 
-function scheduleCheckinsByFrequency(frequency){
+function scheduleCheckinsByFrequency(frequency, time){
 	repo.findCheckinsByFrequency(frequency, function(err, checkins){
 	    checkins.forEach(function(checkin){
-		 	var date = Date.now();
-			var answers = [];
-			var question = checkin.question;
-			var projectName = checkin.project.title;
-			checkin.participants.forEach(function(p){
-				sendEmailToCheckinPartisipants(p, question, projectName);
-				checkin.answers.push({user: p, answer: 'noAnswer', creationDate: date});
-			});
-			checkin.save(function(err, result){
-				if(err){
-					console.log(err);
-				} else {
-					// console.log(result);
-				}
-			});
-			
+	    	var toggle = 0;
+	    	if (checkin.time === time){
+	    		var date = Date.now();
+				var answers = [];
+				var question = checkin.question;
+				var projectName = checkin.project.title;
+				checkin.participants.forEach(function(p){
+					sendEmailToCheckinPartisipants(p, question, projectName);
+					checkin.answers.push({user: p, answer: 'noAnswer', creationDate: date});
+				});
+				checkin.save(function(err, result){
+					if(err){
+						console.log(err);
+					} else {
+						// console.log(result);
+					}
+				});
+	    	}
 		})
 	});
 }
