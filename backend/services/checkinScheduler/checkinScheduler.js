905ec1cd3702eb312849db8f90cefe4d2968c9f0
Yevhen Verba <verba_e_k@mail.ru>
2016-08-29 14:00:41 +0300

    temp

---
+++ backend/services/checkinScheduler/checkinScheduler.js
@@ -0,0 +1,50 @@
+var schedule = require('node-schedule');
+var currentWeekNumber = require('current-week-number');
+var scheduleCheckinsByFrequency = require('./scheduleCheckin');
+
+var everyMonday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 1}, function (){
+	scheduleCheckinsByFrequency('Every Monday')
+});
+
+var everyFriday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 5}, function(){
+	scheduleCheckinsByFrequency('Every Friday')
+});
+
+var everyOtherMonday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 1}, function(){
+	if (currentWeekNumber()%2 === 0){
+		scheduleCheckinsByFrequency('Every other Monday');
+	}
+	
+});
+
+var everyOtherFriday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: 5}, function() {
+	if (currentWeekNumber()%2 === 0){
+		scheduleCheckinsByFrequency('Every other Friday');
+	}
+	
+});
+
+var firstMondayOfEveryMonth = schedule.scheduleJob({hour: 6, minute: 30, dayOfMonth: [new schedule.Range(1, 7)], dayOfWeek: 1}, function() {
+	scheduleCheckinsByFrequency('First Monday of every month');
+});
+
+var everyWeekday = schedule.scheduleJob({hour: 6, minute: 30, dayOfWeek: [new schedule.Range(1, 5)] }, function(){
+	scheduleCheckinsByFrequency('Every weekday')
+});
+
+var test = schedule.scheduleJob('*/2 * * * * *', function(){
+	console.log(new Date());
+	scheduleCheckinsByFrequency('Every Monday');
+});
+
+
+
+module.exports = {
+	everyMonday: everyMonday,
+	everyFriday: everyFriday,
+	everyOtherMonday: everyOtherMonday,
+	everyOtherFriday: everyOtherFriday,
+	firstMondayOfEveryMonth: firstMondayOfEveryMonth,
+	everyWeekday: everyWeekday,
+	test: test
+};
