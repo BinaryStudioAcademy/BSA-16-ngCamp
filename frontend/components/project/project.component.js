e0d4424e408910442cee004429106ffa0b29a505
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-09-02 23:42:46 +0300

    validation on project settings page

--- frontend/components/project/project.component.js
+++ frontend/components/project/project.component.js
@@ -39,115 +39,117 @@ class ProjectComponentController {
         };
     }
 
-        isAdmin() {
-            let self = this;
-            for (let i = 0; i < self.projectAdmins.length; i++){
-                if (self.projectAdmins[i]._id === window._injectedData.userId) self.isUserAdmin = true;
-            }
+    isAdmin() {
+        let self = this;
+        for (let i = 0; i < self.projectAdmins.length; i++) {
+            if (self.projectAdmins[i]._id === window._injectedData.userId) self.isUserAdmin = true;
         }
-        $onInit() {
-            let self = this;
-            self.projectParticipants = [];
+    }
+    $onInit() {
+        let self = this;
+        self.projectParticipants = [];
+        self.httpGeneral.sendRequest({
+            type: "GET",
+            url: "api/user"
+        }).then(function(res) {
+            self.users = res;
             self.httpGeneral.sendRequest({
                 type: "GET",
-                url: "api/user"
+                url: `api/projects/${window._injectedData.currentProject}/withUsers`,
             }).then(function(res) {
-                self.users = res;
-                self.httpGeneral.sendRequest({
-                    type: "GET",
-                    url: `api/projects/${window._injectedData.currentProject}/withUsers`,
-                }).then(function(res) {
-                    self.currentProject = res;
-                    self.projectParticipants = res.participants;
-                    self.projectAdmins = res.admins;
-                    self.dtDeadline = res.endDate;
-                    self.isAdmin();
-                });
+                self.currentProject = res;
+                self.projectParticipants = res.participants;
+                self.projectAdmins = res.admins;
+                self.dtDeadline = res.endDate;
+                self.isAdmin();
             });
-        }
+        });
+    }
 
-        getProjects() {
-            let self = this;
-            self.flag = false;
-            self.httpGeneral.sendRequest(self.userReq).then(function(res) {
-                self.userProjects = res;
-            });
-        }
+    getProjects() {
+        let self = this;
+        self.flag = false;
+        self.httpGeneral.sendRequest(self.userReq).then(function(res) {
+            self.userProjects = res;
+        });
+    }
 
-        setProject() {
-            let self = this;
+    setProject() {
+        let self = this;
 
-            window._injectedData.currentProject = self.currentProjectId;
+        window._injectedData.currentProject = self.currentProjectId;
 
-            self.httpGeneral.sendRequest({
-                type: "PUT",
-                url: `api/user/${window._injectedData.userId}`,
-                body: {
-                    currentProject: self.currentProjectId,
-                }
-            }).then(function(res) {
-                //console.log("Succesfull update currentProject");
-            });
-            this.$onInit();
-        }
+        self.httpGeneral.sendRequest({
+            type: "PUT",
+            url: `api/user/${window._injectedData.userId}`,
+            body: {
+                currentProject: self.currentProjectId,
+            }
+        }).then(function(res) {
+            //console.log("Succesfull update currentProject");
+        });
+        this.$onInit();
+    }
 
-        addParticipator() {
-            let self = this;
-            self.httpGeneral.sendRequest({
-                type: "POST",
-                url: `api/projects/${self.currentProjectId}/participants`,
-                body: {
-                    data: [self.participatorToAdd],
-                },
-            }).then(function(res) {
-                console.log("Succesfull add participator");
-                self.participatorToAdd = "";
-            });
-            self.addParticipatorFlag = false;
-            this.$onInit();
-        }
+    addParticipator() {
+        let self = this;
+        self.httpGeneral.sendRequest({
+            type: "POST",
+            url: `api/projects/${self.currentProjectId}/participants`,
+            body: {
+                data: [self.participatorToAdd],
+            },
+        }).then(function(res) {
+            console.log("Succesfull add participator");
+            self.participatorToAdd = "";
+        });
+        self.addParticipatorFlag = false;
+        this.$onInit();
+    }
 
-        removeParticipant(participator) {
-            let self = this;
-            self.httpGeneral.sendRequest({
-                type: "DELETE",
-                url: `api/projects/${self.currentProjectId}/participants/${participator}`,
-            }).then(function(res) {
-                console.log("Succesfull delete participator");
-            });
-            this.$onInit();
-        }
+    removeParticipant(participator) {
+        let self = this;
+        self.httpGeneral.sendRequest({
+            type: "DELETE",
+            url: `api/projects/${self.currentProjectId}/participants/${participator}`,
+        }).then(function(res) {
+            console.log("Succesfull delete participator");
+        });
+        this.$onInit();
+    }
 
-        addAdmin() {
-            let self = this;
-            self.projectAdmins.push(self.adminToAdd);
-            self.httpGeneral.sendRequest({
-                type: "PUT",
-                url: `api/projects/${self.currentProjectId}`,
-                body: {
-                    admins: self.projectAdmins,
-                },
-            }).then(function(res) {
-                console.log("Succesfull add participator");
-                self.adminToAdd = "";
-            });
-            self.addAdminFlag = false;
-            this.$onInit();
-        }
+    addAdmin() {
+        let self = this;
+        self.projectAdmins.push(self.adminToAdd);
+        self.httpGeneral.sendRequest({
+            type: "PUT",
+            url: `api/projects/${self.currentProjectId}`,
+            body: {
+                admins: self.projectAdmins,
+            },
+        }).then(function(res) {
+            console.log("Succesfull add participator");
+            self.adminToAdd = "";
+        });
+        self.addAdminFlag = false;
+        this.$onInit();
+    }
 
-        removeAdmin(participator) {
-            let self = this;
-            self.httpGeneral.sendRequest({
-                type: "DELETE",
-                url: `api/projects/${self.currentProjectId}/admins/${participator}`,
-            }).then(function(res) {
-                console.log("Succesfull delete participator");
-            });
-            this.$onInit();
-        }
+    removeAdmin(participator) {
+        let self = this;
+        self.httpGeneral.sendRequest({
+            type: "DELETE",
+            url: `api/projects/${self.currentProjectId}/admins/${participator}`,
+        }).then(function(res) {
+            console.log("Succesfull delete participator");
+        });
+        this.$onInit();
+    }
 
-        edit(prop) {
-            let self = this;
+    edit(prop, valid) {
+        let self = this;
+        console.log(valid);
+        if (valid) {
             switch (prop) {
                 case "title":
                     {
@@ -193,44 +195,47 @@ class ProjectComponentController {
                         break;
                     }
             }
-        }
-        modalToggle() {
-            this.modalFlag = !this.modalFlag;
-        }
-
-        deleteProject() {
-            let self = this;
-            self.httpGeneral.sendRequest({
-                type: "DELETE",
-                url: `api/projects/${window._injectedData.currentProject}`
-            }).then(() => {
-                window._injectedData.currentProject = '';
-                self.window.location.reload();
-                self.location.path('/');
-            });
-        }
-        today() {
-            this.dt = new Date();
-        };
-        open() {
-            this.popup.opened = true;
-        }
-        localDate(date) {
-            let newDate = new Date(date);
-            return newDate.toLocaleDateString();
+        } else {
+            self.popupNotifications.notifyError("Please enter new info correctrly");
         }
     }
+    modalToggle() {
+        this.modalFlag = !this.modalFlag;
+    }
 
-    ProjectComponentController.$inject = ['popupNotifications', 'httpGeneral', '$location', '$window'];
-
-    const projectComponent = {
-        controller: ProjectComponentController,
-        selector: 'projectComponent',
-        template: require('./project.component.pug')(),
-        controllerAs: 'proj',
+    deleteProject() {
+        let self = this;
+        self.httpGeneral.sendRequest({
+            type: "DELETE",
+            url: `api/projects/${window._injectedData.currentProject}`
+        }).then(() => {
+            window._injectedData.currentProject = '';
+            self.window.location.reload();
+            self.location.path('/');
+        });
+    }
+    today() {
+        this.dt = new Date();
     };
+    open() {
+        this.popup.opened = true;
+    }
+    localDate(date) {
+        let newDate = new Date(date);
+        return newDate.toLocaleDateString();
+    }
+}
+
+ProjectComponentController.$inject = ['popupNotifications', 'httpGeneral', '$location', '$window'];
+
+const projectComponent = {
+    controller: ProjectComponentController,
+    selector: 'projectComponent',
+    template: require('./project.component.pug')(),
+    controllerAs: 'proj',
+};
 
 
-    export {
-        projectComponent
-    };
+export {
+    projectComponent
+};
