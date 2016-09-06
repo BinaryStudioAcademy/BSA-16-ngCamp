ce8f25d318174316adc821a12df039f8be236093
Yevhen Verba <verba_e_k@mail.ru>
2016-09-06 12:27:48 +0300

    initial

---
+++ frontend/components/rightPanel/rightPanel.component.js
@@ -0,0 +1,27 @@
+﻿//import './checkinsStyles.styl';
+
+class PanelComponentController {
+    constructor(httpGeneral) {
+        this.httpGeneral = httpGeneral;
+    }
+}
+
+PanelComponentController.$inject = [
+    'httpGeneral'
+];
+const rightPanelComponent = {
+    controller: PanelComponentController,
+    selector: 'rightPanelComponent',
+    controllerAs: 'panel',
+    template: require('./panel-pug.component.pug')(),
+    $routeConfig: [{
+        path:'/',
+        name:'Checkins List',
+        component:'checkinsListComponent',
+        useAsDefault: true
+    }]
+};
+
+export {
+    rightPanelComponent
+};
