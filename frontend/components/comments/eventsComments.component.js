d8dde87bde0dbaedfac6a7f235995cf1b18ae60b
Dmitriy Beseda <besedadg@gmail.com>
2016-09-20 13:55:18 +0300

    added avas to event comments

--- frontend/components/comments/eventsComments.component.js
+++ frontend/components/comments/eventsComments.component.js
@@ -3,10 +3,11 @@ import "./commentsStyle.styl";
 
 
 class eventsCommentsComponentController {
-    constructor(httpGeneral, $window, popupNotifications) {
+    constructor(httpGeneral, $window, popupNotifications, userService) {
         this.httpGeneral = httpGeneral;
         this.window = $window;
         this.popupNotifications = popupNotifications;
+        this.userService = userService;
         this.messageId;
         this.comments = [];
         this.myComment;
@@ -14,6 +15,52 @@ class eventsCommentsComponentController {
     $routerOnActivate(next) {
         let self = this;
         self.messageId = next.params.id;
+
+        let async = require('async');
+
+        async.waterfall([
+            function(callback) {
+                vm.userService.getExternalUsersData().then(function(data) {
+                    vm.externalUsersData = data;
+                    callback(null, data);
+                });
+            },
+            function(extUsers, callback) {
+                vm.httpGeneral.sendRequest({
+                    type: "GET",
+                    url: "api/checkins"
+                }).then(function(res) {
+                    vm.httpGeneral.sendRequest({
+                        type: "GET",
+                        url: `/api/checkins/${next.params.id}/withparticipants`
+                    }).then(function(res) {
+                        self.httpGeneral.sendRequest({
+                            type: "GET",
+                            url: `api/event/${next.params.id}/comments`,
+                        }).then(function(res) {
+                            self.comments = res.comments;
+
+                            if (self.externalUsersData && self.externalUsersData.length && self.comments && self.comments.length) {
+                                for (var i = 0; i < self.comments.lengthl i++) {
+                                    if (self.comments[i].author && self.comments[i].author.email) {
+                                        var user = self.userService.getUserByEmail(self.comments[i].author.email, self.externalUsersData);
+                                        if (user.avatar) {
+                                            if (user.avatar.thumbnailUrlAva) self.comments[i].author.avatar = user.avatar.thumbnailUrlAva;
+                                            else(user.avatar.urlAva) self.comments[i].author.avatar = user.avatar.urlAva;
+                                        }
+                                    }
+                                }
+                            }
+                            console.log('comments', self.comments);
+                        });
+                    });
+                    callback(null, null);
+                });
+            }
+        ]);
+
+
+
         self.httpGeneral.sendRequest({
             type: "GET",
             url: `api/event/${next.params.id}/comments`,
@@ -24,7 +71,10 @@ class eventsCommentsComponentController {
     sendComment(valid) {
         let self = this;
         self.comments.push({
-            author: {firstName:window._injectedData.userFirstName,lastName:window._injectedData.userLastName},
+            author: {
+                firstName: window._injectedData.userFirstName,
+                lastName: window._injectedData.userLastName
+            },
             date: new Date(),
             description: self.myComment,
         });
@@ -46,7 +96,7 @@ class eventsCommentsComponentController {
     }
 };
 
-eventsCommentsComponentController.$inject = ['httpGeneral', '$window', 'popupNotifications'];
+eventsCommentsComponentController.$inject = ['httpGeneral', '$window', 'popupNotifications', 'UserService'];
 
 const eventsCommentsComponent = {
     controller: eventsCommentsComponentController,
