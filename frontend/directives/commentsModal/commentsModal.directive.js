1ec5e9ee84d80566b65a241fb014dfd85c94c6ee
AnthStep <lightlife25@gmail.com>
2016-09-23 10:19:24 +0300

    Comments modal Signed-off-by: AnthStep <lightlife25@gmail.com>

--- frontend/directives/commentsModal/commentsModal.directive.js
+++ frontend/directives/commentsModal/commentsModal.directive.js
@@ -6,10 +6,42 @@ class commentsModalController{
         this.modalToggle = false;
         this.http = httpGeneral;
         this.popup = popupNotifications;
+        this.myComment = '';
     }
 
     toggle(){
         this.modalToggle = !this.modalToggle;
+    }
+
+    postMessage(){
+        let self = this;
+        console.log(`api/${self.scope.instanceType}/${self.scope.instanceId}/comment`);
+        if (self.myComment.length) {
+            self.scope.comments.push({
+                author: {
+                    firstName: window._injectedData.userFirstName,
+                    lastName: window._injectedData.userLastName,
+                    avatar: window._injectedData.avatar && window._injectedData.avatar.small ? window._injectedData.avatar.small : ''
+                },
+                date: new Date(),
+                description: self.myComment,
+            });
+            self.http.sendRequest({
+                type: "PUT",
+                url: `api/${self.scope.instanceType}/${self.scope.instanceId}/comment`,
+                body: [{
+                    author: window._injectedData.userId,
+                    date: new Date(),
+                    description: self.myComment,
+                }]
+            }).then(function(res) {
+                console.log("Succesfull send comment");
+                self.myComment = '';
+            });
+        } else {
+            console.log('test');
+            self.popup.notifyError("Please enter comment correctly");
+        }        
     }
 
 
@@ -24,7 +56,8 @@ const commentsModalDirective = {
     controllerAs: 'cm',
     scope: {
         comments: '=',
-        instanceType: '='
+        instanceType: '=',
+        instanceId: '='
     },
     link: commentsModalLink
 };
